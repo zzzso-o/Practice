84bbabcc8490b777afe10f5b60d53ba3e2b08e40
jisoo <1211ditto@gmail.com>
2022-02-26 23:40:58 +0900

    220226

---
+++ d2/L4828_minMAX/4828.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    ai = list(map(int, input().split()))
+
+    max_value, min_value = ai[0], ai[0]
+    for i in range(N):
+        if ai[i] > max_value:
+            max_value = ai[i]
+        elif ai[i] < min_value:
+            min_value = ai[i]
+    print(f'{tc} {max_value - min_value}')

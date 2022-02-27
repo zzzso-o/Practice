6d24f83c0509bd32133ed84c067aa47d32b203b9
jisoo <1211ditto@gmail.com>
2022-02-28 01:48:40 +0900

    2001

---
+++ d2/2001_파리퇴치/2001.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('input.txt')
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+
+    total_list = []
+    for i in range(N-M+1):
+        for j in range(N-M+1):
+            total = 0
+            for dx in range(M):
+                for dy in range(M):
+                    total += arr[i+dx][j+dy]
+            total_list.append(total)
+
+    max_v = total_list[0]
+    for idx in range(len(total_list)):
+        if total_list[idx] > max_v:
+            max_v = total_list[idx]
+
+    print(f'#{tc} {max_v}')

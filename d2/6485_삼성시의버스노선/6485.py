1f6b97dff3fce1e6223a0018c40c019fb0e02439
jisoo <1211ditto@gmail.com>
2022-02-27 23:32:46 +0900

    6485

---
+++ d2/6485_삼성시의버스노선/6485.py
@@ -0,0 +1,14 @@
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    arr = [list(map(int, input().split())) for _ in range(N)]
+    P = int(input())
+    bus_stops = []
+    for _ in range(P):
+        bus_stops.append(int(input()))
+
+    bucket = [0] * 5001
+    for i in range(P):
+        if bus_stops[i] in arr:
+            bucket[i+1] = bus_stops[i]
+    print(f'{tc} {bucket[i+1]}')
--- d2/L4834_숫자카드/4834.py
+++ d2/L4834_숫자카드/4834.py
@@ -6,3 +6,23 @@ for tc in range(1, T+1):
     N = int(input())
     cards = list(map(int, input()))
 
+    cnt = [0] * 10
+    for i in range(10):
+        for j in range(N):
+            if cards[j] == i:
+                cnt[i] += 1
+
+    max_v = cnt[0]
+    max_num = 0
+    for idx in range(10):
+        if cnt[idx] > max_v:
+            max_v = cnt[idx]
+            max_num = idx
+        elif cnt[idx] == max_v:
+            if idx > max_num:
+                max_num = idx
+
+    print(f'{tc} {max_num} {max_v}')
+
+
+
